432062215fe3e2a4fa3ee377332c9ff19169ba3c
ian-lavorente <101892136+ian-lavorente@users.noreply.github.com>
2022-05-11 18:48:31 +0000

    Update 1012.c

--- 1012.c
+++ 1012.c
@@ -1,3 +1,5 @@
+// Área
+
 #include <stdio.h>
 
 int main()
@@ -5,7 +7,6 @@ int main()
 	double A, B, C;
 
 	scanf("%lf %lf %lf", &A, &B, &C);
-
 	printf("TRIANGULO: %.3lf\n", (A * C) / 2);
 	printf("CIRCULO: %.3lf\n", 3.14159 * C * C);
 	printf("TRAPEZIO: %.3lf\n", ((A + B) * C) / 2);

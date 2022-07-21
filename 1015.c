2d7c2332716ea7e95a27f6f0ffaf8036d2165998
ian-lavorente <101892136+ian-lavorente@users.noreply.github.com>
2022-07-21 18:04:38 +0000

    Update 1015.c

--- 1015.c
+++ 1015.c
@@ -6,9 +6,9 @@
 
 int main()
 {
-	float x1, x2, y1, y2;
-	scanf("%f %f", &x1, &y1);
-	scanf("%f %f", &x2, &y2);
-	printf("%.4f\n", sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2)));
-	return 0;
+  float x1, x2, y1, y2;
+  scanf("%f %f", &x1, &y1);
+  scanf("%f %f", &x2, &y2);
+  printf("%.4f\n", sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2)));
+  return 0;
 }
